241f9a719051e7fd3e561d360d68c83b8c6013fd
TarekTolba1 <TarekTolba1@users.noreply.github.com>
2025-11-03 10:19:19 +0000

    Deploy Flutter web from develop: $(date) orca-software-gmbh/bieter_tool_app@40b8a9dc6b629e4bc144076963a88ad17679f344

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -89,9 +89,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "98ad8b31cca37731b91bc29db17c8c15",
+"flutter_bootstrap.js": "96652208f5415673ba59ef751ee78a5e",
 "version.json": "9d5ef0910c9943eceed62e057ed86c45",
-"main.dart.js": "edee9b07b92ba12b9d4a78643b36ed2f"};
+"main.dart.js": "30e494b64d35805b1c823c4e7ab65e7e"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
