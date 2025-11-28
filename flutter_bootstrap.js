0e99e30ac2b98b53a377ff9dad0e4665a41d107d
JulianBissekkou <JulianBissekkou@users.noreply.github.com>
2025-11-28 07:19:54 +0000

    Deploy Flutter web from develop: $(date) orca-software-gmbh/bieter_tool_app@b0bdccec0a18f9b9e4657be4e0d35f2d90dcc568

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3765257665"
+    serviceWorkerVersion: "2243753541"
   }
 });
