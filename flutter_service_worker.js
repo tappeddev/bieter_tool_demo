6fcff41be6b0982ed10a2e005a483cc3a4dcbb34
JulianBissekkou <JulianBissekkou@users.noreply.github.com>
2025-12-04 08:21:06 +0000

    Deploy Flutter web from develop: $(date) orca-software-gmbh/bieter_tool_app@10bf4e4d540ca7376d98f514cfe35b8cc04a907b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -73,7 +73,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "assets/assets/fonts/Roboto-Bold.ttf": "dd5415b95e675853c6ccdceba7324ce7",
 "assets/assets/fonts/Roboto-Medium.ttf": "7d752fb726f5ece291e2e522fcecf86d",
 "assets/fonts/MaterialIcons-Regular.otf": "c0ad29d56cfe3890223c02da3c6e0448",
-"assets/NOTICES": "203713338db41a1d39b81a21fe4f0c7f",
+"assets/NOTICES": "dc651813acfb41fd92258cc02656c077",
 "assets/packages/aad_oauth/assets/msalv2.js": "7fb1a65e14acf2e247a813225eed3ac8",
 "assets/FontManifest.json": "e83983dce1b86afb382d68dd3d139de5",
 "assets/AssetManifest.bin": "5e36fe9f1d5d5d7336b9290ff14e06ab",
@@ -91,9 +91,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "f104cccab9e051ad105a7244d45635fb",
+"flutter_bootstrap.js": "a994e936cf6743aa8c313190e7aa88cd",
 "version.json": "9d5ef0910c9943eceed62e057ed86c45",
-"main.dart.js": "49124adb4e4601b6a3e257efef9a8137"};
+"main.dart.js": "f007158b0d23961709903dc972e935de"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
