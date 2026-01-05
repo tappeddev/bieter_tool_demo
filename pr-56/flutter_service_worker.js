bfd87f88989467f9d15095c2f052f1070bd9423c
TarekTolba1 <TarekTolba1@users.noreply.github.com>
2026-01-05 11:30:26 +0000

    Deploy PR #56 preview orca-software-gmbh/bieter_tool_app@2372bd438e91e008f4afeafb96f4335bc6782932

--- pr-56/flutter_service_worker.js
+++ pr-56/flutter_service_worker.js
@@ -98,9 +98,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "cb1c312fdd14872cffc99410553b6f41",
+"flutter_bootstrap.js": "9be54e64e5436e1cbba59e270471ffeb",
 "version.json": "9d5ef0910c9943eceed62e057ed86c45",
-"main.dart.js": "e3ea7c92822cb05e471cbdbe80f42de2"};
+"main.dart.js": "1877208d6845da544409d0903a0a79fb"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
